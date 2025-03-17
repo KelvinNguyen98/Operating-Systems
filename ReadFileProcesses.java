bf98a760046f9e288301db51c92e9726136903bd
naomidoug <154919799+naomidoug@users.noreply.github.com>
2025-03-17 13:43:17 -0400

    Add files via upload

---
+++ ReadFileProcesses.java
@@ -0,0 +1,30 @@
+/*
+ *  READ TXT FILE AND STORE IN MEMORY
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+
+ import java.io.File; //file class
+ import java.io.FileNotFoundException; // class handles errors
+ import java.util.Scanner; // scanner class to read text files
+
+ public class ReadFile{
+    public static void main(String[] args){
+        try{
+            File myObj = new File("Project1_processes.txt");
+            Scanner myReader = new Scanner(myObj);
+            while (myReader.hasNextLine()){
+                String data = myReader.nextLine();
+                System.out.println(data);
+            }
+            myReader.close();
+        }   catch (FileNotFoundException e){
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+
+    }
+ }
