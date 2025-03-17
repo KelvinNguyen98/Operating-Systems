9c7f89f82a1130d33079048d6876c2b343ad1bee
KelvinNguyen98 <kelvinnguyen132@gmail.com>
2025-03-17 18:23:28 -0400

    Update ReadFile.java

--- Project 1/ReadFile.java
+++ Project 1/ReadFile.java
@@ -11,10 +11,14 @@ public class ReadFile {
    public static void main(String[] args) {
        try {
            File myObj = new File("Project1_processes.txt");
+           // Read data from the text file
            Scanner myReader = new Scanner(myObj);
+           // After reading, it will display that the file was read successfully
            System.out.println("File read successfully");
+           // Close scanner to stop reading from the text file
            myReader.close();
 
+           // If an error occurs, the file will not be read and will display an error message
        }   catch (FileNotFoundException e) {
            System.out.println("Error opening file. Project1_processes.txt is not in the src folder.");
            e.printStackTrace();
