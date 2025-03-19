c91efe20985c663ac37765dbf5e5627b0203cdb5
KelvinNguyen98 <kelvinnguyen132@gmail.com>
2025-03-19 19:14:56 -0400

    Update Main.java

--- Project 1/Main.java
+++ Project 1/Main.java
@@ -12,9 +12,9 @@
 
  import java.io.File;                   // File class
  import java.io.FileNotFoundException;  // Class handles file errors
- import java.util.ArrayList;            // Scanner class to read text files
- import java.util.List;                 // Arrays
- import java.util.Scanner;              // Lists
+ import java.util.ArrayList;            // Arrays
+ import java.util.List;                 // Lists
+ import java.util.Scanner;              // Scanner class to read text files  
 
  // Class to read and process the text file
  // Contributer: Naomi Douglas and Kelvin Nguyen
@@ -138,6 +138,85 @@
     }
  }
 
+ // Class for scheduling algorithm: First-Come, First-Served (FCFS)
+ // Contributer: Kelvin Nguyen
+ class FCFS {
+    private int[][] results; 
+    private int n;
+
+    // Constructor
+    public FCFS(int[][] processData) {
+        this.n = processData.length;
+        // 5 columns: PID, burst time, waiting time, turn around time, start time
+        this.results = new int[n][5]; 
+        execute(processData);
+    }
+
+    // Method to execute FCFS
+    private void execute(int[][] processData) {
+        int totalWT = 0, totalTAT = 0, currentTime = 0;
+
+        // Sort by arrival time
+        java.util.Arrays.sort(processData, java.util.Comparator.comparingInt(p -> p[1]));
+
+        for (int i = 0; i < n; i++) {
+            int PID = processData[i][0];
+            int arrivalTime = processData[i][1];
+            int burstTime = processData[i][2];
+
+            int startTime = Math.max(currentTime, arrivalTime);
+            int waitingTime = startTime - arrivalTime;
+            int turnAroundTime = waitingTime + burstTime; 
+ 
+            results[i][0] = PID;
+            results[i][1] = burstTime;
+            results[i][2] = waitingTime;
+            results[i][3] = turnAroundTime;
+            results[i][4] = startTime;
+
+            currentTime = startTime + burstTime;
+            totalWT += waitingTime;
+            totalTAT += turnAroundTime;
+        }
+
+        // Display Gantt chart
+        GanttChart();
+        // Display Waiting Time (WT), Turn Around Time (TAT), average waiting time, and average turn around time
+        displayTimes(totalWT, totalTAT);
+    }
+
+    // Method to display Gantt chart
+    private void GanttChart() {
+        System.out.println("\nFCFS Gantt Chart");
+        for (int i = 0; i < n; i++) {
+            System.out.printf("|  P%d  ", results[i][0]);
+        }
+        System.out.println("|");
+
+        // Start time of first process
+        System.out.print(results[0][4]);
+        // End time of each process
+        for (int i = 0; i < n; i++) {
+            System.out.print("      " + (results[i][4] + results[i][1])); 
+        }
+        System.out.println();
+    }
+
+    // Method to return Waiting Time (WT), Turn Around Time (TAT), average waiting time, and average turn around time
+    private void displayTimes(int totalWT, int totalTAT) {
+        System.out.println("\nWaiting Time (WT) and Turn Around Time (TAT) for each process:");
+        for (int i = 0; i < n; i++) {
+            System.out.printf("Process P%d: WT = %d, TAT = %d%n", results[i][0], results[i][2], results[i][3]);
+        }
+
+        // Calculate and display average Waiting Time (WT) and Average Turn Around (TAT)
+        double averageWT = (double) totalWT / n;
+        double averageTAT = (double) totalTAT / n;
+        System.out.printf("%nAverage Waiting Time (WT) = %.2f", averageWT);
+        System.out.printf("%nAverage Turn Around Time (AT) = %.2f%n%n", averageTAT);
+    }
+ }
+
  // Main method
  // Contributer: Naomi Douglas and Kelvin Nguyen
  public class Main {
@@ -166,5 +245,13 @@
         for (int[] row : results) {
             System.out.println("P" + row[0] + "\t" + row[1] + "\t" + row[2] + "\t" + row[3]);
         }
+
+        // FCFS scheduling algorithm
+        int[][] fcfs_processes = new int[processList.size()][];
+        for (int i = 0; i < processList.size(); i++) {
+            fcfs_processes[i] = processList.get(i);
+        }
+        // Execute FCFS
+        FCFS fcfs = new FCFS(fcfs_processes);
     } 
 }
