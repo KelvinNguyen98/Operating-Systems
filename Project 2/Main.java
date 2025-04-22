5f9fbac42634ce6e6e9e95f6e730ae52dab9d3c2
KelvinNguyen98 <kelvinnguyen132@gmail.com>
2025-04-22 15:34:50 -0400

    Update Main.java

--- Project 2/Main.java
+++ Project 2/Main.java
@@ -62,8 +62,23 @@
         }
         return processList;
     }
+ }
 
-    
+ // Taken from PDF of project
+ // Thread to simulate process
+ class ProcessThread extends Thread {
+    int pid, burstTime;
+    public ProcessThread(int pid, int burstTime) {
+        this.pid = pid;
+        this.burstTime = burstTime;
+    }
+    public void run() {
+        System.out.println("Process " + pid + " started.");
+        try {
+            Thread.sleep(burstTime * 1000);
+        } catch (InterruptedException e) {}
+        System.out.println("Process " + pid + " finished.");
+    }
  }
 
  // Main Method
