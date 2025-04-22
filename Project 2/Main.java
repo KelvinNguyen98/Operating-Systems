1d636799f53c3a4732fa069a10cabcb1d821f06d
KelvinNguyen98 <kelvinnguyen132@gmail.com>
2025-04-22 14:39:33 -0400

    Add files via upload

---
+++ Project 2/Main.java
@@ -0,0 +1,18 @@
+/*
+ * Author: Naomi Douglas and Kelvin Nguyen
+ * 
+ * This program will simulate real-time process execution using threads
+ * and how it handles synchronization
+ * 
+ * The synchronization problem used: Producer-Consumer
+ * It will use mutexes and semaphores
+ * 
+ */
+
+// Main method
+// Contributer: Naomi Douglas and Kelvin Nguyen
+public class Main {
+    public static void main(String[] args) {
+        // Main code
+    }
+}
