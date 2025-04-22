c5a246cba62a1505ffa834f49e7a25960312a638
KelvinNguyen98 <kelvinnguyen132@gmail.com>
2025-04-22 14:57:02 -0400

    Update Main.java

--- Project 2/Main.java
+++ Project 2/Main.java
@@ -6,7 +6,13 @@
  * 
  * The synchronization problem used: Producer-Consumer
  * It will use mutexes and semaphores
+ * 
+ * Sources: geeksforgeeks
  */
+
+ import java.util.*;            // Arrays, Lists, etc. 
+ import java.util.concurrent.*; // Semaphore and and ReentrantLock
+ import java.io.*;              // File, Exception, etc. 
 
 // Main method
 // Contributer: Naomi Douglas and Kelvin Nguyen
